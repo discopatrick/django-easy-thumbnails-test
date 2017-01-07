82dc9f398b73b7a2bf07628a8ba63e485253625b
discopatrick <git@discopatrick.me>
2017-01-07 21:58:28 +0000

    adds image pregeneration

--- django_easy_thumbnails_test/images/models.py
+++ django_easy_thumbnails_test/images/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+from easy_thumbnails.signals import saved_file
+from easy_thumbnails.signal_handlers import generate_aliases_global
+
+saved_file.connect(generate_aliases_global)
+
 class Person(models.Model):
     image = models.ImageField()
 
